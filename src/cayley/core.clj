2f7b373a3a489f786c99a19a8fb1189f2dcf95ce
Aaron Decker <deckeraa@uwplatt.edu>
2014-04-11 20:33:29 -0500

    Made a protocol for groups. Moved d4 into a record for the group protocol. Hope this works out. Much of the old code needs refactored now. For example, the multi-variadic star code needs reworked.

--- src/cayley/core.clj
+++ src/cayley/core.clj
@@ -1,5 +1,35 @@
-(ns cayley.core)
+(ns cayley.core
+  (:use [clojure.test :as test]))
 
+;; (defprotocol P
+;;   (foo [x])
+;;   (bar-me [x] [x y]))
+
+;; (bar-me
+;;  (let [x 42]
+;;    (reify P
+;;      (foo [this] 17)
+;;      (bar-me [this] x)
+;;      (bar-me [this meow] (+ x meow)))) -42)
+
+;; (defrecord test-P [offset]
+;;   P
+;;   (foo [this] offset)
+;;   (bar-me [this] offset)
+;;   (bar-me [this woof] (+ woof offset)))
+
+;; (bar-me (test-P. 23) 17)
+;; (test-P.foo 4)
+
+(defprotocol group
+  "Protocol for defining elements and how the elements operate in a group"
+  (elems [this] "Returns a set of the elements of the group")
+  (operate [this elem1 elem2] "Operates elem1 with elem2"))
+
+(defn operate-via-cayley-table [table elem1 elem2]
+  (get (get table elem1) elem2))
+
+;; d4 -- symmetries of a square, using my abstract algebra professor's custom notation
 (def d4-elems #{:iota :phi :psi :theta :sigma :tau :lambda :mu} )
 (def d4-cayley
   {:iota {:iota :iota, :phi :phi, :psi :psi, :theta :theta, :sigma :sigma, :tau :tau, :lambda :lambda, :mu :mu }
@@ -11,8 +41,19 @@
    :lambda {:iota :lambda, :phi :mu, :psi :sigma, :theta :tau, :sigma :psi, :tau :theta, :lambda :iota, :mu :phi }
    :mu {:iota :mu, :phi :lambda, :psi :tau, :theta :sigma, :sigma :theta, :tau :psi, :lambda :phi, :mu :iota }})
 
-;; (defn star [table elem1 elem2]
-;;   (get (get table elem1) elem2))
+(defrecord d4 []
+  group
+  (elems [this] #{:iota :phi :psi :theta :sigma :tau :lambda :mu})
+  (operate [this elem1 elem2] (operate-via-cayley-table d4-cayley elem1 elem2)))
+
+(deftest test-d4-elem (is (= (elems (d4.)) #{:iota :phi :psi :theta :sigma :tau :lambda :mu})))
+(deftest test-d4-1(is (= (operate (d4.) :sigma :tau) :iota)))
+
+;; integer groups -- addition modulo n
+;; (defrecord int-group [n]
+;;   "Integer group modulo n"
+;;   group
+;;   (elems [this] ))
 
 ;; Operate elements together. First argument is the Cayley table as above.
 (def star
@@ -21,6 +62,8 @@
        (get (get table elem1) elem2))
     ([table elem1 elem2 & more-elems]
        (apply this table (this table elem1 elem2) more-elems))))
+
+(deftest star1 (is (= (star d4-cayley :psi :lambda) :tau)))
 
 (defn inverse [table elem]
   (:iota (clojure.set/map-invert (get table elem))))
@@ -59,44 +102,44 @@
                      (operate-many table elements))]
     (if (= elements newly-found)
       elements
-      (new-cycle-to-set table newly-found))))
+      (cycle-to-set table newly-found))))
 
 ;; Evaluate these for 21.22
-(normal? d4-cayley (cycle-to-set d4-cayley #{:iota}) d4-elems)
-(normal? d4-cayley (cycle-to-set d4-cayley #{:theta}) d4-elems)
-(normal? d4-cayley (cycle-to-set d4-cayley #{:psi}) d4-elems)
-(normal? d4-cayley (cycle-to-set d4-cayley #{:phi}) d4-elems)
-(normal? d4-cayley (cycle-to-set d4-cayley #{:lambda}) d4-elems)
-(normal? d4-cayley (cycle-to-set d4-cayley #{:mu}) d4-elems)
-(normal? d4-cayley (cycle-to-set d4-cayley #{:theta :psi}) d4-elems)
-(normal? d4-cayley (cycle-to-set d4-cayley #{:tau}) d4-elems)
-(normal? d4-cayley (cycle-to-set d4-cayley #{:lambda :mu}) d4-elems)
-(normal? d4-cayley (cycle-to-set d4-cayley d4-elems) d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley #{:iota}) d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley #{:theta}) d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley #{:psi}) d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley #{:phi}) d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley #{:lambda}) d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley #{:mu}) d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley #{:theta :psi}) d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley #{:tau}) d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley #{:lambda :mu}) d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley d4-elems) d4-elems)
 
 ;; playground -- these should really get turned into unit tests somehow
-(map #(list (str :theta " conj " %1) (conjugate d4-cayley :theta %1)) d4-elems)
-(gen-single-conjugation-map d4-cayley :theta d4-elems)
-(gen-conjugation-maps d4-cayley #{:theta :psi} d4-elems)
-(normal? d4-cayley #{:theta} d4-elems)
-(normal? d4-cayley (cycle-to-set d4-cayley :theta) d4-elems)
-(conjugate d4-cayley :psi :lambda)
-(contains? #{:theta} :theta)
-(star d4-cayley :psi :lambda)
-(star d4-cayley :lambda :tau)
-(star d4-cayley :sigma :theta :tau)
-(star d4-cayley :lambda :psi :lambda)
-(conjugate d4-cayley :theta :tau)
-(star d4-cayley :tau :theta :sigma)
-((partial star d4-cayley) :tau :lambda)
+;; (map #(list (str :theta " conj " %1) (conjugate d4-cayley :theta %1)) d4-elems)
+;; (gen-single-conjugation-map d4-cayley :theta d4-elems)
+;; (gen-conjugation-maps d4-cayley #{:theta :psi} d4-elems)
+;; (normal? d4-cayley #{:theta} d4-elems)
+;; (normal? d4-cayley (cycle-to-set d4-cayley :theta) d4-elems)
+;; (conjugate d4-cayley :psi :lambda)
+;; (contains? #{:theta} :theta)
 
-(inverse d4-cayley :tau)
+;; (star d4-cayley :lambda :tau)
+;; (star d4-cayley :sigma :theta :tau)
+;; (star d4-cayley :lambda :psi :lambda)
+;; (conjugate d4-cayley :theta :tau)
+;; (star d4-cayley :tau :theta :sigma)
+;; ((partial star d4-cayley) :tau :lambda)
 
-(take-while #(not= :iota %1) (rest (iterate #(star d4-cayley %1 :theta) :iota)))
-(cycle-to-set d4-cayley :tau)
+;; (inverse d4-cayley :tau)
 
-(clojure.set/union (cycle-to-set d4-cayley :theta) (cycle-to-set d4-cayley :tau))
-(for [x #{:tau :sigma}
-      y #{:iota :phi}]
-  (star d4-cayley x y))
-(operate-many d4-cayley (operate-many d4-cayley #{:tau :phi}))
-(cycle-to-set d4-cayley #{:lambda :mu})
+;; (take-while #(not= :iota %1) (rest (iterate #(star d4-cayley %1 :theta) :iota)))
+;; (cycle-to-set d4-cayley :tau)
+
+;; (clojure.set/union (cycle-to-set d4-cayley :theta) (cycle-to-set d4-cayley :tau))
+;; (for [x #{:tau :sigma}
+;;       y #{:iota :phi}]
+;;   (star d4-cayley x y))
+;; (operate-many d4-cayley (operate-many d4-cayley #{:tau :phi}))
+;; (cycle-to-set d4-cayley #{:lambda :mu})
