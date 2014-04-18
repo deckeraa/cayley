1cf3aeaec5a4a2675531ee648b38e5971db06147
Aaron Decker <deckeraa@uwplatt.edu>
2014-04-18 12:34:06 -0500

    Added a static css file with very ugly formatting. I think I will move to dynamically generated css with the Garden library in the near future.

--- src/cayley/handler.clj
+++ src/cayley/handler.clj
@@ -10,18 +10,19 @@
 
 (defroutes app-routes
   (GET "/" [] (html/html [:html
+                          [:link {:rel "stylesheet" :type "text/css" :href "/css/group.css"}]
                           (group-info (core/direct-product. (core/int-group. 4) (core/int-group. 2)))
-]))
-  (route/resources "/")
+                          ]))
+  (route/resources "/" {:root "."} )
   (route/not-found "Not Found"))
 
 (def app
   (handler/site app-routes))
 
 (defn group-info [group]
-  [:div#group-info
-   [:div#group-description
+  [:div.group-info
+   [:div.group-description
     (str (core/group-name group) ": " (core/group-string group))]
-   [:div#order-table
+   [:div.order-table
     [:p "Order Table"]
     (core/order-table-to-html-hiccup group)]])
